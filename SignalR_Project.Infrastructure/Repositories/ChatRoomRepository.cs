29c4d8dd2dad79933e6da3ce1040ee9b0c97ef95
Eda Nur Üretir (BilgeAdam Boost) <edanur.uretir@bilgeadamboost.com>
2025-09-01 15:44:13 +0300

    repolar oluşturuldu

---
+++ SignalR_Project.Infrastructure/Repositories/ChatRoomRepository.cs
@@ -0,0 +1,13 @@
+﻿using SignalR_Project.Core.Entities;
+using SignalR_Project.Core.Interfaces;
+using SignalR_Project.Infrastructure.Contexts;
+
+namespace SignalR_Project.Infrastructure.Repositories
+{
+    public class ChatRoomRepository : EfRepository<ChatRoom>, IChatRoomRepository
+    {
+        public ChatRoomRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
